afd149e19167712ee60140ba9b9fee507f214327
motahugo <99303278+motahugo@users.noreply.github.com>
2022-03-13 21:48:48 -0300

    Deploy
    Versão concluído da parte visual

---
+++ window.py
@@ -0,0 +1,170 @@
+from tkinter import *
+
+
+def btn_clicked():
+    print("Button Clicked")
+
+
+window = Tk()
+
+window.geometry("773x774")
+window.configure(bg = "#ffffff")
+canvas = Canvas(
+    window,
+    bg = "#ffffff",
+    height = 774,
+    width = 773,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge")
+canvas.place(x = 0, y = 0)
+
+background_img = PhotoImage(file = f"background.png")
+background = canvas.create_image(
+    386.5, 387.0,
+    image=background_img)
+
+entry0_img = PhotoImage(file = f"img_textBox0.png")
+entry0_bg = canvas.create_image(
+    466.0, 672.5,
+    image = entry0_img)
+
+entry0 = Text(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry0.place(
+    x = 258, y = 619,
+    width = 416,
+    height = 105)
+
+entry1_img = PhotoImage(file = f"img_textBox1.png")
+entry1_bg = canvas.create_image(
+    465.0, 570.0,
+    image = entry1_img)
+
+entry1 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry1.place(
+    x = 257, y = 553,
+    width = 416,
+    height = 32)
+
+entry2_img = PhotoImage(file = f"img_textBox2.png")
+entry2_bg = canvas.create_image(
+    465.0, 510.0,
+    image = entry2_img)
+
+entry2 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry2.place(
+    x = 257, y = 493,
+    width = 416,
+    height = 32)
+
+entry3_img = PhotoImage(file = f"img_textBox3.png")
+entry3_bg = canvas.create_image(
+    465.0, 447.0,
+    image = entry3_img)
+
+entry3 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry3.place(
+    x = 257, y = 430,
+    width = 416,
+    height = 32)
+
+entry4_img = PhotoImage(file = f"img_textBox4.png")
+entry4_bg = canvas.create_image(
+    466.0, 392.0,
+    image = entry4_img)
+
+entry4 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry4.place(
+    x = 258, y = 375,
+    width = 416,
+    height = 32)
+
+entry5_img = PhotoImage(file = f"img_textBox5.png")
+entry5_bg = canvas.create_image(
+    465.0, 332.0,
+    image = entry5_img)
+
+entry5 = Entry(
+    bd = 0,
+    bg = "#ffffff",
+    highlightthickness = 0)
+
+entry5.place(
+    x = 257, y = 315,
+    width = 416,
+    height = 32)
+
+img0 = PhotoImage(file = f"img0.png")
+b0 = Button(
+    image = img0,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b0.place(
+    x = 193, y = 213,
+    width = 208,
+    height = 57)
+
+img1 = PhotoImage(file = f"img1.png")
+b1 = Button(
+    image = img1,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b1.place(
+    x = 492, y = 136,
+    width = 209,
+    height = 62)
+
+img2 = PhotoImage(file = f"img2.png")
+b2 = Button(
+    image = img2,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b2.place(
+    x = 192, y = 136,
+    width = 209,
+    height = 55)
+
+img3 = PhotoImage(file = f"img3.png")
+b3 = Button(
+    image = img3,
+    borderwidth = 0,
+    highlightthickness = 0,
+    command = btn_clicked,
+    relief = "flat")
+
+b3.place(
+    x = 492, y = 215,
+    width = 209,
+    height = 55)
+
+window.resizable(False, False)
+window.mainloop()
